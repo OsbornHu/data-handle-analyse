68033f93c0b63f6e9f2f9f0ae005e2d572f37550
osbornHu <hujian@foreveross.com>
2019-11-25 09:16:11 +0000

    no message

---
+++ mongodb_select.sql
@@ -0,0 +1,9 @@
+select a.code,a.name from growthData a  where  a.mbrg>70 and a.nprg > 70
+
+select * from stockbasic where code='300586'
+
+select * from stockbasic 
+
+select * from disclosureDate
+
+drop collection disclosureDate
